ff4ca7c3ace68cd78af5ef07bfd2d681d56a8eca
serhiihrytsunyk <serhii.hrytsunyk@gmail.com>
2023-03-08 00:08:47 +0200

    ok

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-const event = require('node:events');
-
-const eventEmitter = new event();
+//
 //
 // eventEmitter.on('click', ()=>{
 //     console.log('click!!!!')
@@ -24,10 +22,104 @@ const eventEmitter = new event();
 // eventEmitter.emit('Click&DIe')
 // eventEmitter.emit('Click&DIe')
 
-const fs = require('fs');
-const path = require('node:path');
+// const fs = require('fs');
+// const path = require('path');
+//
+//
+//
+//
+// const readStream = fs.createReadStream(path.join(process.cwd(),'hello','text.txt'));
+// const wrightStream = fs.createWriteStream(path.join('hello','text2.txt'));
+//
+// readStream.pipe(wrightStream)
 
-fs.mkdir(path.join(process.cwd(),'test'))
+const express = require('express');
 
+const app = express();
+const PORT = 5200;
+//
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-// const readStream = fs.createReadStream(path())
+app.get('/welcome', (req, res) => {
+    res.send('WELCOME!!!🌝🌝🌝')
+    res.end()
+})
+
+const users = [
+    {
+        name: 'Vasya',
+        age: 25,
+        gender: 'male'
+    },
+    {
+        name: 'Petya',
+        age: 15,
+        gender: 'male'
+    },
+    {
+        name: 'Masha',
+        age: 35,
+        gender: 'female'
+    },
+    {
+        name: 'Aton',
+        age: 5,
+        gender: 'male'
+    },
+    {
+        name: 'Ira',
+        age: 20,
+        gender: 'female'
+    },
+]
+
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.get('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+
+    const user = users[userId - 1];
+
+    res.send(user)
+})
+
+app.post('/users', (req, res) => {
+    const body = req.body;
+    users.push(body)
+
+    res.status(201).json({
+        message: 'user created'
+    })
+})
+
+app.put('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+    const updatedUser = req.body;
+
+    users[+userId - 1] = updatedUser;
+
+    res.status(200).json({
+        message: 'user updated',
+        data: users[+userId - 1]
+    })
+
+})
+
+app.delete('/users/:userId', (req, res)=>{
+    const {userId} = req.params;
+
+    users.splice(+userId - 1,1)
+
+    res.status(200).json({
+        message: 'User deleted!'
+    })
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server runs on PORT ${PORT} 🌝️`)
+})
+
